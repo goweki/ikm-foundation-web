dc40391c788292453e9ba96118ce1a249db7a716
G. Oweki <contact@goweki.com>
2025-05-24 17:03:29 +0300

    ...

--- src/app/api/ui/route.ts
+++ src/app/api/ui/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export const dynamic = "auto";
-export const revalidate = 7200;
+export const revalidate = 7200; // 7200 seconds = 2 hours
 
 const airtableBaseID = process.env.AIRTABLE_BASE_ID;
 const airtablePAT_read = process.env.AIRTABLE_PAT_READ;
