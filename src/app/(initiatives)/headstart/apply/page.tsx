e9ef03bb59a39896849cdf344b9ea033c506bc48
G. Oweki <contact@goweki.com>
2025-06-12 17:22:17 +0300

    headstart form

--- src/app/(initiatives)/headstart/apply/page.tsx
+++ src/app/(initiatives)/headstart/apply/page.tsx
@@ -1,20 +1,15 @@
-import ApplyEducationForm from "@/components/forms/ApplyEducation";
 import FormLayout from "../../_components/formLayout";
 
 export default function HeadStart() {
   return (
-    <FormLayout
-      page="headstart"
-      // descLink={{ label: "Learn more", link: "/apply/scholarship" }}
-    >
-      <ApplyEducationForm applicationFor="headstart" />
-      {/* <div className="mt-6 text-center">
-        <div className="flex items-center justify-center space-x-2">
-          <span className="h-px w-16 bg-slate-500" />
-          <span className="text-default-500 text-sm">Learn more</span>
-          <span className="h-px w-16 bg-slate-500" />
-        </div>
-      </div> */}
+    <FormLayout page="headstart">
+      <iframe
+        className="airtable-embed"
+        src="https://airtable.com/embed/appGQZtROtfry9TUl/pagO48MkzMYYF6y2B/form"
+        width="100%"
+        height="533"
+        style={{ background: "transparent", border: "1px solid #ccc" }}
+      />
     </FormLayout>
   );
 }
