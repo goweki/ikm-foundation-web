6766480283ce143d1a160bf8fbc36334917d94b4
G. Oweki <contact@goweki.com>
2025-07-04 05:27:10 +0300

    ....

--- src/components/Aboutus/index.tsx
+++ src/components/Aboutus/index.tsx
@@ -107,25 +107,53 @@ const Aboutus = () => {
 
           <div className="flex flex-wrap items-center justify-center mt-10 lg:mt-16 gap-4 lg:gap-8">
             {Aboutdata.map((item, i) => (
-              <a
+              <div
                 key={i}
-                href={item.href}
-                className="py-2 md:py-4 flex flex-col items-center border border-gray-200 rounded-lg shadow-sm hover:shadow-lg md:flex-row md:max-w-xl hover:bg-linear-to-r hover:from-cyan-100 hover:to-blue-200 hover:translate-y-1 transition-all duration-200 ease-in-out"
+                className="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12"
               >
-                <img
-                  className="object-cover w-full h-full rounded-t-lg p-4 lg:p8 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                  src={item.imgSrc}
-                  alt="key-area-img"
-                />
-                <div className="flex flex-col justify-between p-4 leading-normal max-w-xs">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight">
-                    {item.heading}
-                  </h5>
-                  <p className="mb-3 font-normal min-h-[72px]">
-                    {item.paragraph}
-                  </p>
+                <div className="group relative overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+                  <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                  <div className="relative z-10 mx-auto max-w-md">
+                    <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
+                      {/* <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-10 w-10 text-white transition-all"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                        />
+                      </svg> */}
+                      <img
+                        className="object-cover w-full h-full rounded-t-lg p-4 lg:p8 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                        src={item.imgSrc}
+                        alt="key-area-img"
+                      />
+                    </span>
+                    <div className="space-y-6 pt-5 font-bold text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+                      <p>{item.heading}</p>
+                    </div>
+                    <p className="text-gray-600 font-thin transition-all duration-300 group-hover:text-white/90">
+                      {item.paragraph}
+                    </p>
+                    <div className="pt-5 text-base font-semibold leading-7">
+                      <p>
+                        <a
+                          href={item.href}
+                          className="text-blue-500 text-lg transition-all duration-300 group-hover:text-white"
+                        >
+                          Apply &rarr;
+                        </a>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </a>
+              </div>
             ))}
           </div>
         </div>
