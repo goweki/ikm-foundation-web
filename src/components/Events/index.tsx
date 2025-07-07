f8ef9181a6ba2e6591fe739db3a56fd906fc8b9e
G. Oweki <contact@goweki.com>
2025-07-07 16:00:50 +0300

    styling updates

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -78,22 +78,16 @@ class MultipleItems extends Component<Props> {
                         height={262}
                         className="inline-block m-auto rounded-lg"
                       />
-                      <h3 className="absolute top-4 left-4 bg-blue-900 text-white py-3 px-6 rounded-lg">
+                      <h3 className="absolute text-xs md:text-sm top-4 left-4 bg-blue-900 text-white py-3 px-6 rounded-lg">
                         {event.date}
                       </h3>
-                      <h4 className="text-2xl font-bold pt-6 px-1 text-black">
+                      <h4 className="text-base md:text-xl font-bold pt-6 px-1 text-black">
                         {event.name}
                       </h4>
-                      {/* <h4 className="text-2xl font-bold pt-1 text-black">
-                      {items.description}
-                    </h4> */}
                       <div>
-                        <h3 className="text-base font-normal py-2 opacity-75 px-1">
+                        <h3 className="text-sm md:text-base font-normal py-2 opacity-75 px-1">
                           {event.description}
                         </h3>
-                        {/* <h3 className="text-base font-normal pb-1 opacity-75">
-                        {items.date}
-                      </h3> */}
                       </div>
                     </div>
                   </div>
