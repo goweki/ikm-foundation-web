38aa4215c75102a2f7025eb773078a95cb71a52e
G. Oweki <contact@goweki.com>
2025-07-18 07:43:09 +0300

    coming soon update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,143 @@
-export default function Home() {
+"use client";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+
+interface TimeLeft {
+  days?: number;
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+}
+
+const calculateTimeLeft = (targetDate: Date): TimeLeft => {
+  const difference = +targetDate - +new Date();
+  let timeLeft: TimeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+  return timeLeft;
+};
+
+export default function LandingSection() {
+  const targetDate = React.useMemo(() => {
+    const date = new Date("2025-12-12T00:00:00Z");
+    return date;
+  }, []);
+
+  const [timeLeft, setTimeLeft] = useState<TimeLeft>({});
+  const [launched, setLaunched] = useState<boolean>(false);
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+
+    const initialTimeLeft = calculateTimeLeft(targetDate);
+    setTimeLeft(initialTimeLeft);
+    const isLaunchedInitially =
+      (initialTimeLeft.days === 0 || initialTimeLeft.days === undefined) &&
+      (initialTimeLeft.hours === 0 || initialTimeLeft.hours === undefined) &&
+      (initialTimeLeft.minutes === 0 ||
+        initialTimeLeft.minutes === undefined) &&
+      (initialTimeLeft.seconds === 0 || initialTimeLeft.seconds === undefined);
+
+    if (isLaunchedInitially) {
+      setLaunched(true);
+    }
+
+    const timer = setInterval(() => {
+      const newTimeLeft = calculateTimeLeft(targetDate);
+      setTimeLeft(newTimeLeft);
+
+      const isLaunchedNow =
+        (newTimeLeft.days === 0 || newTimeLeft.days === undefined) &&
+        (newTimeLeft.hours === 0 || newTimeLeft.hours === undefined) &&
+        (newTimeLeft.minutes === 0 || newTimeLeft.minutes === undefined) &&
+        (newTimeLeft.seconds === 0 || newTimeLeft.seconds === undefined);
+
+      if (isLaunchedNow) {
+        setLaunched(true);
+        clearInterval(timer);
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [targetDate]);
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="container">
-        <div className="rounded-lg shadow-lg p-5 md:p-20 mx-2">
-          <div className="text-center">
-            <h2 className="text-xl md:text-3xl mt-10 uppercase font-bold mb-4">
-              Coming Soon...
-            </h2>
-            <h3 className="text-4xl tracking-tight leading-10 font-extrabold sm:text-5xl sm:leading-none md:text-6xl">
-              IKM <span className="text-indigo-500">Foundation</span>
-            </h3>
-            <p className="text-md md:text-xl mt-10 mb-4">
-              currently under mainternance by
-            </p>
-            <p>
-              <a
-                href="https://www.goweki.com/"
-                title="Goweki"
-                className="md:w-32 tracking-wide  font-bold rounded border-2 border-indigo-500 hover:border-indigo-500 hover:bg-indigo-500 shadow-md py-2 px-6 inline-flex items-center"
-              >
-                <span className="mx-auto">Goweki</span>
-              </a>
-            </p>
-          </div>
+    <section className="banner-bg min-h-screen flex items-center justify-center text-white p-4 sm:p-8">
+      <div className="max-w-md mx-auto text-center bg-blue-500/10 backdrop-blur-sm rounded-xl shadow-lg p-12">
+        <Image
+          src="/logo_onDark.png"
+          alt="Logo"
+          height={124}
+          width={124}
+          className="mx-auto mb-4 rounded-full"
+        />
+
+        <p className="text-lg md:text-2xl text-gray-300 mb-2 font-inter">
+          IKM Foundation
+        </p>
+
+        <h1 className="text-4xl md:text-6xl sm:text-5xl font-extrabold text-white mb-6 font-inter">
+          Coming Soon
+        </h1>
+
+        <div
+          className={`
+            countdown flex justify-center items-center space-x-4 sm:space-x-6
+            text-2xl sm:text-3xl font-bold rounded-lg p-4 shadow bg-blue-800/50
+            ${launched ? "text-teal-400" : "text-white"}
+          `}
+        >
+          {launched ? (
+            <div>Launched!</div>
+          ) : (
+            <>
+              {isClient &&
+              (timeLeft.days !== undefined ||
+                timeLeft.hours !== undefined ||
+                timeLeft.minutes !== undefined ||
+                timeLeft.seconds !== undefined) ? (
+                <>
+                  <div className="flex flex-col items-center">
+                    {timeLeft.days}
+                    <span className="text-sm font-normal text-gray-400">
+                      Days
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center">
+                    {timeLeft.hours}
+                    <span className="text-sm font-normal text-gray-400">
+                      Hours
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center">
+                    {timeLeft.minutes}
+                    <span className="text-sm font-normal text-gray-400">
+                      Minutes
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center">
+                    {timeLeft.seconds}
+                    <span className="text-sm font-normal text-gray-400">
+                      Seconds
+                    </span>
+                  </div>
+                </>
+              ) : (
+                <div>Loading Countdown...</div>
+              )}
+            </>
+          )}
         </div>
       </div>
-    </div>
+    </section>
   );
 }
