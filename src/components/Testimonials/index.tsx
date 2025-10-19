aeb49e47b7008b3f5ba1f06a7614abf80c44ab8c
Goweki <contact@goweki.com>
2025-10-19 18:09:29 +0300

    reworks impact

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,109 +1,3 @@
-// import TestimonialsSlider from "./_testimonials";
-
-// export default function Testimonials() {
-//   return (
-//     <section className="bg-testimonial pt-40 pb-32 lg:py-33" id="testimonials">
-//       {/* <div className="mx-auto max-w-7xl sm:py-4 lg:px-8">
-//         <div className="text-center">
-//           <h3
-//             className="text-4xl sm:text-6xl font-bold text-black my-3"
-//             data-aos="fade-up"
-//             data-aos-anchor-placement="top-center"
-//             data-aos-duration="1000"
-//           >
-//             Stories of Impact
-//           </h3>
-
-//           <h3
-//             className="text-4xl sm:text-6xl font-bold text-black/50 lg:mr-48 my-4"
-//             data-aos="fade-up"
-//             data-aos-anchor-placement="top-center"
-//             data-aos-duration="1100"
-//           >
-//             Stories of Impact
-//           </h3>
-
-//           <h3
-//             className="text-4xl sm:text-6xl font-bold text-black/25 opacity-25 lg:-mr-32 my-4"
-//             data-aos="fade-up"
-//             data-aos-anchor-placement="top-center"
-//             data-aos-duration="1000"
-//           >
-//             Stories of Impact
-//           </h3>
-//         </div>
-//         <TestimonialsSlider />
-//       </div> */}
-//       <div className="text-center mb-6 md:mb-12">
-//         <h3
-//           className="text-4xl sm:text-6xl font-bold text-black my-3"
-//           data-aos="fade-up"
-//           data-aos-anchor-placement="top-center"
-//           data-aos-duration="1000"
-//         >
-//           Stories of Impact
-//         </h3>
-
-//         <h3
-//           className="text-4xl sm:text-6xl font-bold text-black/50 lg:mr-48 my-4"
-//           data-aos="fade-up"
-//           data-aos-anchor-placement="top-center"
-//           data-aos-duration="700"
-//         >
-//           Stories of Impact
-//         </h3>
-
-//         <h3
-//           className="text-4xl sm:text-6xl font-bold text-black/25 opacity-25 lg:-mr-32 my-4"
-//           data-aos="fade-up"
-//           data-aos-anchor-placement="top-center"
-//           data-aos-duration="500"
-//         >
-//           Stories of Impact
-//         </h3>
-//       </div>
-//       <div className="container max-w-xl mx-auto">
-//         <div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-50 dark:text-gray-800">
-//           <img
-//             src="https://source.unsplash.com/random/100x100?4"
-//             alt=""
-//             className="w-20 h-20 rounded-full dark:bg-gray-500"
-//           />
-//           <blockquote className="max-w-lg text-lg italic font-medium text-center">
-//             "Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae?
-//             Rem unde atque mollitia!"
-//           </blockquote>
-//           <div className="text-center dark:text-gray-600">
-//             <p>Leroy Jenkins</p>
-//             <p>CEO of Company Co.</p>
-//           </div>
-//           <div className="flex space-x-2">
-//             <button
-//               type="button"
-//               aria-label="Page 1"
-//               className="w-2 h-2 rounded-full dark:bg-gray-900"
-//             ></button>
-//             <button
-//               type="button"
-//               aria-label="Page 2"
-//               className="w-2 h-2 rounded-full dark:bg-gray-400"
-//             ></button>
-//             <button
-//               type="button"
-//               aria-label="Page 3"
-//               className="w-2 h-2 rounded-full dark:bg-gray-400"
-//             ></button>
-//             <button
-//               type="button"
-//               aria-label="Page 4"
-//               className="w-2 h-2 rounded-full dark:bg-gray-400"
-//             ></button>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
 "use client";
 
 import { DataContext, Impact } from "@/app/providers";
@@ -113,7 +7,18 @@ import TestimonialCards from "./_testimonialCards";
 
 export default function Testimonials() {
   const dataContext = useContext(DataContext);
-  const impact: Impact[] = dataContext?.data?.impact || [];
+
+  // const impact: Impact[] = dataContext?.data?.impact || [];
+  const impact: { quantity: string; description: string }[] = [
+    { quantity: "2,000", description: "beneficiaries" },
+    { quantity: "5", description: "projects" },
+    {
+      quantity: "47M+",
+      description: "KES invested",
+    },
+    { quantity: "230", description: "Scholars" },
+  ];
+
   return (
     <section className="" id="testimonials">
       <div className="container px-6 py-12 mx-auto">
@@ -155,13 +60,12 @@ export default function Testimonials() {
                 <div className="grid grid-cols-1 md:grid-cols-3 mx-5 gap-14 md:gap-6 text-white">
                   {impact.map((item, i) => (
                     <div key={i} className="h-fit rounded-xl p-6">
-                      <div className="font-semibold mb-1 text-lg md:text-base text-blue-200 border-b">
-                        {item.label}
-                      </div>
-                      <div className="font-semibold text-5xl md:text-2xl tracking-tight ">
+                      <div className="font-semibold text-5xl md:text-2xl tracking-tight border-b">
                         {item.quantity}+
                       </div>
-                      <div className="font-normal opacity-75">{item.unit}</div>
+                      <div className="font-normal opacity-75">
+                        {item.description}
+                      </div>
                     </div>
                   ))}
                 </div>
